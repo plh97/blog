bec40b5357e332e4a5b6b23e3f453d73f2bcfeee
pengliheng <pengliheng111@gmail.com>
2017-10-20 22:09:47 +0800

    golange's usage of Switch

--- src/jsx/blog/golang/backendgo.jsx
+++ src/jsx/blog/golang/backendgo.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { tomorrowNightEighties,xcode } from "react-syntax-highlighter/dist/styles";
-import { Row, Col } from 'antd';
+import {tomorrowNightEighties, xcode} from "react-syntax-highlighter/dist/styles";
+import {Row, Col} from 'antd';
 
 export default class backendgo extends React.Component {
   constructor() {
     super();
     this.state = {
-      language: '',
+      language: 'go',
       style: tomorrowNightEighties,
       showLineNumbers: true
     };
@@ -18,7 +18,35 @@ export default class backendgo extends React.Component {
     const style = this.state.style;
     return (
       <Col span={24}>
-        <h3>Go catch me</h3>
+        <h3>Go ---Switch Usage</h3>
+        <SyntaxHighlighter showLineNumbers={showLineNumbers} language={language} style={style}>{`package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  today := time.Now().Weekday()
+  // “:=”符号Go语言会自动识别today变量的类别，这点和typescript类似
+  fmt.Println("什么时候放假？")
+  switch time.Saturday {
+    //Fantasy ---> time.Now().Weekday()+1 ====>   "Wednesday" === "Thursday" - 1
+    case today - 2:
+    	fmt.Println("假日已度过。。。")
+    case today - 1:
+    	fmt.Println("昨天都已经放假了！")
+    case today + 0:
+    	fmt.Println("今天放假！")
+    case today + 1:
+    	fmt.Println("明天放假~！")
+    case today + 2:
+    	fmt.Println("后天放假~~！")
+    default:
+    	fmt.Println("想太多~~~！")
+  }
+}`
+}</SyntaxHighlighter>
       </Col>
     )
   }
