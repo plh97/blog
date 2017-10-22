e59b1a8d6c902ddea2b40df816402b8bea694043
pengliheng <pengliheng111@gmail.com>
2017-10-22 15:12:54 +0800

    to set blog to https

---
+++ apps.js
@@ -0,0 +1,33 @@
+const http = require('https')
+const App = require('koa');
+const app = new App()
+// SSL options
+// const fs = require('fs');
+// const options = {
+//     key: fs.readFileSync('./peng.pipk.top.key'),  //ssl文件路径
+//     cert: fs.readFileSync('./peng.pipk.top.crt')  //ssl文件路径
+// };
+
+// start the server
+const server = http.createServer(app.callback());
+const static = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const router = require('koa-router')();
+const webpack = require('webpack');
+const webpackMiddleware = require('koa-webpack-dev-middleware');
+const staticPath = './dist';
+console.log("process.env.NODE_ENV :",process.env.NODE_ENV )
+
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(require('koa-static')(staticPath))
+
+
+servers.listen(443);
+// server.listen(8001);
+// const config = require('./webpack.config')
+// app.use(webpackMiddleware(webpack(config), {
+//   stats: {colors: true}
+// }));
