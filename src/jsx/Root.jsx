35dd870b935557cdd81dd1695e1a9379ec08538a
pengliheng <pengliheng111@gmail.com>
2017-11-02 01:18:49 +0800

    123

--- src/jsx/Root.jsx
+++ src/jsx/Root.jsx
@@ -59,7 +59,7 @@ export default class Root extends React.Component {
             <Menu.Item key="github" style={{float:"right"}}>
               <iframe
           	    style={{marginLeft: "2px", marginBottom:"-5px"}}
-          	    frameborder="0" scrolling="0" width="91px" height="20px"
+          	    frameBorder="0" scrolling="0" width="91px" height="20px"
           	    src="https://ghbtns.com/github-btn.html?user=pengliheng&repo=pengliheng.github.io&type=star&count=true" >
           		</iframe>
             </Menu.Item>
