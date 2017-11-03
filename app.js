b3620b421cf75b7ba97a11981ed6b45cf1f00cc8
pengliheng <pengliheng111@gmail.com>
2017-11-03 16:29:17 +0800

    compress

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app
     extname: 'html',
     commands: {}
   }))
-  app.use(compress({
-    filter: function (content_type) {
-    	return /text/i.test(content_type)
-    },
-    threshold: 2048,
-    flush: require('zlib').Z_SYNC_FLUSH
-  }))
+  // app.use(compress({
+  //   filter: function (content_type) {
+  //   	return /text/i.test(content_type)
+  //   },
+  //   threshold: 2048,
+  //   flush: require('zlib').Z_SYNC_FLUSH
+  // }))
 
 app.use(async(ctx, next) => {
   await ctx.render('index', {});
