1bcd5021801a440f55d2c1aca38bc2866d6c7c65
pengliheng <pengliheng111@gmail.com>
2018-02-21 23:05:38 +0800

    update init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,8 +10,7 @@ module.exports = {
 			},
 			env_prod: {
 				"PORT": 8001,
-				"NODE_ENV": "prod",
-				"TOKEN": "ed8cc3810c26e5f4b386982a2b99408da425be7d",
+				"NODE_ENV": "prod"
 			}
 		}
 	]
