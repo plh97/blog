34e169872da286c128fc83d58b242ed85d55a74c
pengliheng <pengliheng111@gmail.com>
2018-03-06 01:05:23 +0800

    refresh dist

--- src/client/jsx/blog/github/index.jsx
+++ src/client/jsx/blog/github/index.jsx
@@ -1,6 +1,7 @@
 // package
 import React, { Component } from 'react';
 import Github from '@pengliheng/github-report';
+import '@pengliheng/github-report/lib/index.less';
 // import Github from './Github.jsx';
 
 // local
