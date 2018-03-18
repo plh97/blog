4033707e824549859b36411f18600c6faabc8218
pengliheng <pengliheng111@gmail.com>
2018-03-18 19:19:21 +0800

    123

--- procfile.js
+++ procfile.js
@@ -1,9 +1,21 @@
 'use strict';
 
-module.exports = (pandora) => {
+
+module.exports = function (pandora) {
   pandora
+
+    // 定义一个进程，名字叫 processA
+    .process('processA')
+
+    // 如果 scale 大于 1 ，将使用 Node.js 的 Cluster 模块自动产生进程组
+    // 默认值即是 1
+    .scale(1)
+
+    // 定义进程环境变量，创建出来的进程中可以通过 process.env 获得
     .env({
       PORT: '8001',
     })
+
+  // 这个进程的入口文件地址
     .entry('./src/server/index.js');
 };
