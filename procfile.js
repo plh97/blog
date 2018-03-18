446377d01fe49e4221f161be424a10510ec0b4c8
pengliheng <pengliheng111@gmail.com>
2018-03-18 19:02:39 +0800

    123

--- procfile.js
+++ procfile.js
@@ -1,33 +1,7 @@
 'use strict';
 
 module.exports = (pandora) => {
-
   pandora
+    .process('blog')
     .cluster('./src/server/index.js');
-    
-  /** 
-  * you can custom workers scale number
-  */
-  // pandora
-  //   .process('worker')
-  //   .scale(2); // .scale('auto') means os.cpus().length
-
-  /**
-   * you can also use fork mode to start application 
-   */
-  // pandora
-  //   .fork('blog', './src/server/index.js');
-
-  /**
-   * you can create another process here
-   */
-  // pandora
-  //   .process('background')
-  //   .nodeArgs(['--expose-gc']);
-
-  /**
-   * more features please visit our document.
-   * https://github.com/midwayjs/pandora/
-   */
-
 };
