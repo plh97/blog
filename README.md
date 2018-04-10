0d4e7d92572b51fed9103caccadadf978ea070b3
penliheng <pengliheng111@gmail.com>
2018-04-10 20:56:59 +0800

    2e1

--- README.md
+++ README.md
@@ -31,6 +31,19 @@
 - ~~Vue搭建前端页面~~
 - 代码片段Gist
 - 分析Github相关数据
+- vue mvvm
+- vue/react diff 算法
+- mock数据分层如何实现
+- webpack js模块化打包机制，他如何实现的import,require然后引入对应js代码？
+- webpack loader写一个？
+- vue如何实现mvvm的，
+- react/vuediff算法如何实现的
+- jquery工程模式如何实现的
+- 我擅长的事情？？？
+- 解释一下原型链，new后面内部做了什么事情
+- http了解过吗？
+- 以后基础多补补吧。
+- 没事，至少坚持写文章还是很不错的。
 
 
 ### 特性
@@ -59,3 +72,41 @@ yarn run nodemon      # 监听10086端口
 ```
 **Welcome PR 😀**
 
+
+-----BEGIN CERTIFICATE-----
+MIIGOjCCBSKgAwIBAgISAxnfHC8K8GptLNktDQBXt94jMA0GCSqGSIb3DQEBCwUA
+MEoxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MSMwIQYDVQQD
+ExpMZXQncyBFbmNyeXB0IEF1dGhvcml0eSBYMzAeFw0xODA0MTAwMTQ3NDJaFw0x
+ODA3MDkwMTQ3NDJaMBMxETAPBgNVBAMTCHBpcGsudG9wMIIBIjANBgkqhkiG9w0B
+AQEFAAOCAQ8AMIIBCgKCAQEA2aQADUk0liLtd8+htkyrylHrpBKtKxKVRyt8480P
+bLltTT4rj5032Fr4NeSi4dTLuebTeFZCxZbGQ69CIhYEzkH9U6RDWLSwWUXSxGSc
+F83GADAAmi+plvOKVR4DL6twKOivZ2aMXpRlw6Ce7lsPmeys9oOzVX+mp1Td+9wc
+c3MolWMwb6BRc15bXyHQuiGOvLPHkSPsz3YuVYXXR9KcgzSfi393xfnHSVEL3Vuv
+/inbqRpXaFHCkxSOrL4W8lKLiI/JUYqvCh7IG4CtUDxh4FV+P5tPffjFPkJ/vUXV
+H4j6qYx+tooZubTq0XZeDKZ374ACWHRb2IaqYJth46woPwIDAQABo4IDTzCCA0sw
+DgYDVR0PAQH/BAQDAgWgMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAM
+BgNVHRMBAf8EAjAAMB0GA1UdDgQWBBQJUNPssHWyuhxUKoXa9v+DCQvrWzAfBgNV
+HSMEGDAWgBSoSmpjBH3duubRObemRWXv86jsoTBvBggrBgEFBQcBAQRjMGEwLgYI
+KwYBBQUHMAGGImh0dHA6Ly9vY3NwLmludC14My5sZXRzZW5jcnlwdC5vcmcwLwYI
+KwYBBQUHMAKGI2h0dHA6Ly9jZXJ0LmludC14My5sZXRzZW5jcnlwdC5vcmcvMFEG
+A1UdEQRKMEiCDWJsb2cucGlway50b3CCDWNoYXQucGlway50b3CCEGtpbmd1Ym8u
+cGlway50b3CCCHBpcGsudG9wggx3d3cucGlway50b3Awgf4GA1UdIASB9jCB8zAI
+BgZngQwBAgEwgeYGCysGAQQBgt8TAQEBMIHWMCYGCCsGAQUFBwIBFhpodHRwOi8v
+Y3BzLmxldHNlbmNyeXB0Lm9yZzCBqwYIKwYBBQUHAgIwgZ4MgZtUaGlzIENlcnRp
+ZmljYXRlIG1heSBvbmx5IGJlIHJlbGllZCB1cG9uIGJ5IFJlbHlpbmcgUGFydGll
+cyBhbmQgb25seSBpbiBhY2NvcmRhbmNlIHdpdGggdGhlIENlcnRpZmljYXRlIFBv
+bGljeSBmb3VuZCBhdCBodHRwczovL2xldHNlbmNyeXB0Lm9yZy9yZXBvc2l0b3J5
+LzCCAQUGCisGAQQB1nkCBAIEgfYEgfMA8QB3ANt0r+7LKeyx/so+cW0s5bmquzb3
+hHGDx12dTze2H79kAAABYq1zsI4AAAQDAEgwRgIhAI6aNILZROKCAbqrC6Zt4xlq
+Djs4kqe49IWHMcsA2c1TAiEAiPv41lMov5vWF4PdD+gUow8x14G0dq9J9box/jeY
+mHQAdgApPFGWVMg5ZbqqUPxYB9S3b79Yeily3KTDDPTlRUf0eAAAAWKtc7ChAAAE
+AwBHMEUCIBmakAp9GvkUdD5FGs84lCLSM9XcFFAjD95wz4VQyzXCAiEA0X+rbTzw
+fEg/IXbFaMXVP8LnOal/ddBYINT8yV05ZpQwDQYJKoZIhvcNAQELBQADggEBAAa0
+eFdczWMBCGXpo7gHEWb8KC4MxdzSVBUDYbsNpiW+eZzFzcvYh/dXn99xGjaPlVp3
+/Xygrp8Wtjv7ahe4wj3EcWZ7GLg2iYq1QRs1kFT9X7ZQqZKlZs5uSbBY6XfZNKan
+Z/gwpG7XWtGub61oqKwIghnXtXPrkjCbqNglN5q5xz4XEML6Wodl7ThK1RBl7ROq
+gjAKSo4H4MDw3m3qapV17k8mcscRWF+aGroEfJJeBFW19N146aK7SI2TcYEqcM10
+NbnLIff5pMWzIdOQmblP5/CfzeqPYs3MSLifw3exI3Gu5hh5IdPbQhPtCeMwoJ8i
+CXxwdd544QsqFzs4Qsg=
+-----END CERTIFICATE-----
+acme.sh --issue -d pipk.top -d '*.pipk.top' --dns
--- src/client/jsx/blog/others/nginx.jsx
+++ src/client/jsx/blog/others/nginx.jsx
@@ -18,86 +18,58 @@ const Nginx = () => (
       </li>
     </ol>
     <PrismCode lang="nginx" >{`
-        http {
-          # http://*.penlh.com -----> https://*.penlh.com ------> http://112.74.63.84:8080
-          #                                                 |---> http://112.74.63.84:8001
-          include                    mime.types;
-          default_type               application/octet-stream;
-          sendfile                   on;
-          keepalive_timeout          65;
-          # 用于开启Gzip压缩，至少可以将文件压缩至1/3，极大加快首次加载速度
-          gzip                       on;      
-          gzip_disable               "msie6";
-          gzip_vary                  on;
-          gzip_proxied               any;
-          # 压缩比率，1~9，数值越大，压缩体积越小，服务器消耗事件越长
-          gzip_comp_level            6;       
-          gzip_buffers               16 8k;
-          # http1.1  可选http1.0   .. 根据你的个人要求来设置
-          gzip_http_version          1.1;     
-          gzip_min_length            256;
-          gzip_types                 text/css application/x-javascript application/javascript text/javascript;
-          # https配置
-          server {
-            listen                   443 ssl;
-            # 所适配的域名， 如果域名为以下，则应用如下配置
-            # you can use your cmd to test your https whether work ...  eg:..
-            # curl -I https://www.penlh.com
-            # Server: nginx/1.13.6
-            # Date: Fri, 27 Oct 2017 20:23:47 GMT
-            # Content-Type: text/html; charset=utf-8
-            # Content-Length: 19429
-            # Connection: keep-alive
-            # Vary: Accept-Encoding
-            # Last-Modified: Fri, 27 Oct 2017 11:57:59 GMT
-            # Cache-Control: max-age=30758
-            server_name              penlh.com www.penlh.com blog.penlh.com;
-            # 建议使用 https://certbot.eff.org
-            # 傻瓜式自动生成，免费证书，并且可选择多个子域名同时应用该证书，具体可以去他的官网看一下
-            ssl_certificate          /etc/letsencrypt/live/penlh.com/fullchain.pem; # managed by Certbot
-            ssl_certificate_key      /etc/letsencrypt/live/penlh.com/privkey.pem; # managed by Certbot
-            include                  /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-            ssl_dhparam              /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-            # -------------------------------all above is certbot auto create------------------------------
-            ssl_session_cache         shared:SSL:1m;
-            # i use the reverse proxy to both 8001 and 8080 prot such as this below
-            # so that you can just easy creat http with 8001 port and use nginx's feature reverse proxy
-            location / {
-              proxy_pass              http://127.0.0.1:8001;
-              proxy_http_version      1.1;
-              proxy_set_header        Upgrade $http_upgrade;
-              proxy_set_header        Connection "upgrade";
-              proxy_set_header        Host $host;
-            }
-          }
-          # all the same as above config just with chat.penlh.com => 8080 prot
-          # note ::::
-          # i suggest not to use if or if...else...,it may cause a bug that your all got a 403 page
-          # want to know more your can read https://nginx.org/en/docs/http/converting_rewrite_rules.html
-          # to learn the difficult rules
-          server {
-            listen                   443 ssl;
-            server_name              chat.penlh.com;
-            ssl_certificate          /etc/letsencrypt/live/penlh.com/fullchain.pem; # managed by Certbot
-            ssl_certificate_key      /etc/letsencrypt/live/penlh.com/privkey.pem; # managed by Certbot
-            include                  /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-            ssl_dhparam              /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-            ssl_session_cache         shared:SSL:1m;
-            location / {
-              proxy_pass              http://127.0.0.1:8002;
-              proxy_http_version      1.1;
-              proxy_set_header        Upgrade $http_upgrade;
-              proxy_set_header        Connection "upgrade";
-              proxy_set_header        Host $host;
-            }
-          }
-          # this can rewrite http ==> https   with all the domain like this www.penlh.com penlh.com blog.penlh.com .....
-          server {
-            listen                    80;
-            server_name               *.penlh.com;
-            rewrite                   ^(.*)$ https://$host$1 permanent;
-          }
-        }
+http {
+  include                    mime.types;
+  default_type               application/octet-stream;
+  sendfile                   on;
+  keepalive_timeout          65;
+  gzip                       on;      
+  gzip_disable               "msie6";
+  gzip_vary                  on;
+  gzip_proxied               any;
+  gzip_comp_level            6;       
+  gzip_buffers               16 8k;
+  gzip_http_version          1.1;     
+  gzip_min_length            256;
+  gzip_types                 text/css application/x-javascript application/javascript text/javascript;
+  # https配置
+  server {
+    listen                   443 ssl;
+    server_name              pipk.top www.pipk.top blog.pipk.top;
+
+    ssl_certificate          /etc/letsencrypt/live/pipk.top/fullchain.pem; # managed by Certbot
+    ssl_
+    certificate_key      /etc/letsencrypt/live/pipk.top/privkey.pem; # managed by Certbot
+    include                  /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam              /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+    ssl_session_cache         shared:SSL:1m;
+    location / {
+      proxy_pass              http://127.0.0.1:8001;
+      proxy_http_version      1.1;
+      proxy_set_header        Upgrade $http_upgrade;
+      proxy_set_header        Connection "upgrade";
+      proxy_set_header        Host $host;
+    }
+  }
+    include                  /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam              /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+    ssl_session_cache         shared:SSL:1m;
+    location / {
+      proxy_pass              http://127.0.0.1:8002;
+      proxy_http_version      1.1;
+      proxy_set_header        Upgrade $http_upgrade;
+      proxy_set_header        Connection "upgrade";
+      proxy_set_header        Host $host;
+    }
+  }
+
+  server {
+    listen                    80;
+    server_name               *.pipk.top;
+    rewrite                   ^(.*)$ https://$host$1 permanent;
+  }
+}
       `}
     </PrismCode>
   </div>
