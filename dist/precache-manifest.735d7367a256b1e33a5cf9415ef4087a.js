5edc5a6bf7245642ccf96d29a0aee395d006f903
pengliheng <pengliheng111@gmail.com>
2018-05-18 17:07:28 +0800

    1e2

--- dist/precache-manifest.735d7367a256b1e33a5cf9415ef4087a.js
+++ dist/precache-manifest.735d7367a256b1e33a5cf9415ef4087a.js
@@ -3,11 +3,11 @@ self.__precacheManifest = [
     "url": "/vendor.64568923032218f4d313.js"
   },
   {
-    "revision": "6735259bc00f94b910c7320195dda54d",
+    "revision": "6b8b7dbe9623f466248e9bdeff275fc1",
     "url": "/index.html"
   },
   {
-    "url": "/index.60e3d195f7cbfdc26579.css"
+    "url": "/index.aa05745653c2b1ee96a7.css"
   },
   {
     "revision": "b900986ec0b2e480a2abfb1418af1bb0",
@@ -17,7 +17,7 @@ self.__precacheManifest = [
     "url": "/ed20bba30e23ea60213e9fe13281c10f.woff"
   },
   {
-    "url": "/app.60e3d195f7cbfdc26579.js"
+    "url": "/app.aa05745653c2b1ee96a7.js"
   },
   {
     "url": "/0.59b3f1c10ac78eed688b.js"
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7cada3a3267476769641cd97082af792.js"
+  "/precache-manifest.735d7367a256b1e33a5cf9415ef4087a.js"
 );
 
 workbox.skipWaiting();
