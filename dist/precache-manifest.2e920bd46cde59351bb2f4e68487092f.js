bfe5d274444d552807e47d985570c0b8b01c13b3
pengliheng <pengliheng111@gmail.com>
2018-05-24 22:56:33 +0800

    after fixed @pengliheng/github-report,need to update this package

--- dist/precache-manifest.2e920bd46cde59351bb2f4e68487092f.js
+++ dist/precache-manifest.2e920bd46cde59351bb2f4e68487092f.js
@@ -1,13 +1,13 @@
 self.__precacheManifest = [
   {
-    "url": "/vendor.949cd395f6f9b1b6883e.js"
+    "url": "/vendor.d016d2ed59900da26acf.js"
   },
   {
-    "revision": "fb7d2ddad15108d7784a635de9431c0e",
+    "revision": "a4c198879d5d25e23fdaa2c5afaf9012",
     "url": "/index.html"
   },
   {
-    "url": "/index.ae8298cd5494781b5785.css"
+    "url": "/index.619fe78d9ad0a9ad7710.css"
   },
   {
     "revision": "b900986ec0b2e480a2abfb1418af1bb0",
@@ -17,7 +17,7 @@ self.__precacheManifest = [
     "url": "/ed20bba30e23ea60213e9fe13281c10f.woff"
   },
   {
-    "url": "/app.ae8298cd5494781b5785.js"
+    "url": "/app.619fe78d9ad0a9ad7710.js"
   },
   {
     "url": "/0.7e4323feb49d67e1e2dd.js"
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.673d8a803d90171160f39bfbd672fcd4.js"
+  "/precache-manifest.2e920bd46cde59351bb2f4e68487092f.js"
 );
 
 workbox.skipWaiting();
