b83c445373d2b93c595d9d48e49786b78026122d
pengliheng <pengliheng111@gmail.com>
2019-02-10 20:10:43 +0800

    add dockjer

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -15,7 +15,6 @@ class HomePage extends Component {
     super()
     this.state={
       viewer: '',
-
     }
   }
   componentDidMount() {
@@ -72,32 +71,32 @@ class HomePage extends Component {
       // Prismjs.highlightAll();
     });
   }
-  uploadFile = e => {
-    console.log(e.target.files);
-    Array.prototype.slice.call(e.target.files).filter(file=>{
-      if (!file.type.match(/image/)) {
-        console.log(`${file.name}不是图片,无法上传`);
-        return false
-      } else if (file.size > 1024*1024*2) {
-        console.log(`${file.name}大于2MB,请换一张小点的图片`);
-        return false
-      } else {
-        const form = new FormData();
+  // uploadFile = e => {
+  //   console.log(e.target.files);
+  //   Array.prototype.slice.call(e.target.files).filter(file=>{
+  //     if (!file.type.match(/image/)) {
+  //       console.log(`${file.name}不是图片,无法上传`);
+  //       return false
+  //     } else if (file.size > 1024*1024*2) {
+  //       console.log(`${file.name}大于2MB,请换一张小点的图片`);
+  //       return false
+  //     } else {
+  //       const form = new FormData();
 
-        form.append('file',file)
-        axios({
-          // url: 'http://localhost:3001/upload',
-          url: 'https://api.pipk.top/upload',
-          method: 'post',
-          data: form,
-        })
-        return true;
-      }
-    }).forEach(file=>{
-      console.log(file);
+  //       form.append('file',file)
+  //       axios({
+  //         // url: 'http://localhost:3001/upload',
+  //         url: 'https://api.pipk.top/upload',
+  //         method: 'post',
+  //         data: form,
+  //       })
+  //       return true;
+  //     }
+  //   }).forEach(file=>{
+  //     console.log(file);
 
-    })
-  }
+  //   })
+  // }
   render() {
     return (
       <div className="HomePage-container">
