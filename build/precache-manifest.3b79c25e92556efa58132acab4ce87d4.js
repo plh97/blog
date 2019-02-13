6d3dc55471567275183af31171c04077a4591125
pengliheng <pengliheng111@gmail.com>
2019-02-14 01:54:01 +0800

    test

---
+++ build/precache-manifest.3b79c25e92556efa58132acab4ce87d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8e20e59f519fa85722",
+    "url": "/static/css/main.e51e74b5.chunk.css"
+  },
+  {
+    "revision": "aa8e20e59f519fa85722",
+    "url": "/static/js/main.aa8e20e5.chunk.js"
+  },
+  {
+    "revision": "fadb3b6c69669aacc5e0",
+    "url": "/static/js/1.fadb3b6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed20bba30e23ea60213e9fe13281c10f",
+    "url": "/static/media/a.ed20bba3.woff"
+  },
+  {
+    "revision": "4bd5aba977f821fe8e86a102a0d50d44",
+    "url": "/static/media/cruise.4bd5aba9.woff"
+  },
+  {
+    "revision": "145ba96cf43f58d0b628e4c7a2953127",
+    "url": "/static/media/cruise.145ba96c.ttf"
+  },
+  {
+    "revision": "b0a319befd157d350047f2950ee87a98",
+    "url": "/static/media/cruise.b0a319be.svg"
+  },
+  {
+    "revision": "d2190883f307bb4eae0380bd448bae26",
+    "url": "/index.html"
+  }
+];
