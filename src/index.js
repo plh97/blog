5d865ff148a87a760748f1e0becbcecbb0b134d1
pengliheng <pengliheng111@gmail.com>
2019-03-28 20:36:01 +0800

    fix: travis did not support css import with webpack alias

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux'
 // local
 import App from '@/App'
 import '@/common/index.scss'
+import '@/common/markdown.css'
+import '@/assets/font-icons/fonts.css'
 import store from '@/redux-relate/store'
 
 ReactDOM.render(
