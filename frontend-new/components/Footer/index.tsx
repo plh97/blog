46f273d2800e5a3d43932ab147317a6b56f0c968
plh97 <pengliheng111@gmail.com>
2023-10-09 13:19:53 +0800

    feat: 🎸 footer

--- frontend-new/components/Footer/index.tsx
+++ frontend-new/components/Footer/index.tsx
@@ -5,9 +5,9 @@ export default function Footer () {
 	return (
 		<div className="Footer-container">
 			©<span>Copyright</span>
-			<span>2017-2019</span>
+			<span>2017-2023</span>
 			<span>
-				<b>pipk.top</b>
+				<b>plh-blog.netlify.app</b>
 			</span>
 		</div>
 	)
