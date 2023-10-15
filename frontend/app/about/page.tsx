2a9ae8ba4808092245b524aa583beb939cb02e9e
plh97 <pengliheng111@gmail.com>
2023-10-15 17:07:30 +0800

    feat: deploy frontend

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -10,6 +10,11 @@ import Viewer from "@/components/Viewer";
 import Markdown from "@/components/Markdown";
 import { fetchRepositoryDetail } from "@/apis/repository";
 
+export const metadata = {
+  title: "About Me",
+  description: "This Page is About Me",
+};
+
 export default async function About() {
   const userRes = await fetchUser();
   const repositoryRes = await fetchRepositoryDetail({
