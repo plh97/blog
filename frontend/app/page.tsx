c79814f0357ec3fef428ec273e6bb71653d39333
plh97 <pengliheng111@gmail.com>
2023-10-16 10:09:01 +0800

    feat: side menu high

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
-import { fetchHome } from "@/apis/home";
 import { fetchUser } from "@/apis/user";
 import Viewer from "@/components/Viewer";
 import Markdown from "@/components/Markdown";
 
 import './page.scss'
+import { fetchRepositoryDetail } from "@/apis/repository";
+import _ from "lodash";
 
 export const metadata = {
   title: 'Home Page',
@@ -12,14 +13,23 @@ export const metadata = {
 
 export default async function Home() {
   const user = await fetchUser();
-  const home = await fetchHome();
+  const repositoryRes = await fetchRepositoryDetail({
+    repo: 'plh97/blog',
+    branch: 'develop'
+  });
+  console.log(repositoryRes);
+  const repositoryText = _.get(
+    repositoryRes,
+    "data.search.edges[0].node.object.text",
+    ""
+  );
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 HomePage">
       <Viewer title="主页" data={user.data.viewer} />
       <Markdown
         className="markdown-body HomePage-markdown__body"
       >
-        {home.data.repositoryOwner.repository.object.text}
+        {repositoryText}
       </Markdown>
     </main>
   );
