4faaf18f4846b9fd64f48909ed1429df95cebfc4
plh97 <pengliheng111@gmail.com>
2023-10-18 21:09:01 +0800

    feat: add page not found

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 import styles from "./not-found.module.scss";
 import Link from "next/link";
 
-const PageNotFound = () => (
-  <div className={styles["page"]}>
-    <Image className={styles["img"]} alt="not_found" src={NotFoundSvg} />
-    <p>Could not find requested resource</p>
-    <Link href="/">Return Home</Link>
-  </div>
-);
-
-export default PageNotFound;
+export default async function PageNotFound() {
+  return (
+    <div className={styles["page"]}>
+      <Image className={styles["img"]} alt="not_found" src={NotFoundSvg} />
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
