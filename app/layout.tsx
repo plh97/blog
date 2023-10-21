fcb2797887ef5222102a3b54ee479c32000957c6
plh97 <pengliheng111@gmail.com>
2023-10-21 17:28:37 +0800

    feat: support image render

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 // import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
-
 import styles from "./layout.module.scss";
 import Side from "@/components/Side";
 import Footer from "@/components/Footer";
-import './globals.css'
+import Head from "next/head";
+import "./globals.css";
 
 export default function RootLayout({
   children,
@@ -12,6 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
       <body>
         {/* <ProgressBar /> */}
         <div className={styles["container"]}>
