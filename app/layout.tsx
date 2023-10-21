6bb69d72c1455ca8085504a5fec6759b8cf8b003
plh97 <pengliheng111@gmail.com>
2023-10-21 16:31:03 +0800

    feat: support image render

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </Head>
       <body suppressHydrationWarning>
-        <ProgressBar />
+        {/* <ProgressBar /> */}
         <div className={styles["container"]}>
           <div className={styles["content"]}>
             <div className={styles["side"]}>
