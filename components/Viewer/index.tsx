782d25dd5c01bb5b900afa659ee407e1dc7bb96d
plh97 <pengliheng111@gmail.com>
2023-10-22 16:17:01 +0800

    feat: support image render

--- components/Viewer/index.tsx
+++ components/Viewer/index.tsx
@@ -5,31 +5,33 @@ import { IViewer } from "@/apis/user";
 import classNames from "classnames";
 
 interface IProps {
-	data: IViewer;
-	title: string;
+  data: IViewer;
+  title: string;
 }
 
 export default function Viewer({ data, title = "" }: IProps) {
-	return (
-		<div className={styles["Viewer"]}>
-			<div className={styles["container"]}>
-				<h1 className="z-10">{title}</h1>
-				<div className={classNames(styles["detail"], 'z-10')}>
-					{data?.avatarUrl && (
-						<Image
-							className={styles["image"]}
-							alt="avatar"
-							width={64}
-							height={64}
-							src={data?.avatarUrl}
-						/>
-					)}
-					<span className={styles["detail-list"]}>
-						<span className={styles["detail-name"]}>{data.name}</span>
-						<span className={styles["detail-bio"]}>{data.bio}</span>
-					</span>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles["Viewer"]}>
+      <div className={styles["container"]}>
+        <h1 className="mt-4 text-2xl font-extrabold text-gray-900">{title}</h1>
+        <div className={classNames(styles["detail"], "z-10")}>
+          {data?.avatarUrl && (
+            <Image
+              className={styles["image"]}
+              alt="avatar"
+              width={64}
+              height={64}
+              src={data?.avatarUrl}
+            />
+          )}
+          <div className={styles["detail-list"]}>
+            <h2 className={"text-4xl"}>
+              {data.name}
+            </h2>
+            <p>{data.bio}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
