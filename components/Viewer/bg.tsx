2f11a21a030a24f0bafdc8b7ec3ee21f3a80cbfc
plh97 <pengliheng111@gmail.com>
2023-12-03 20:18:38 +0800

    feat: udpate css

--- components/Viewer/bg.tsx
+++ components/Viewer/bg.tsx
@@ -1,24 +1,25 @@
 import classNames from "classnames";
-// import BG from "@/public/bg.svg";
+import BG from "@/public/bg.svg";
 import Image from "next/image";
 
 export default function CssBackground(props: {
   children: JSX.Element | JSX.Element[];
   className?: string;
 }) {
+  console.log(BG);
   return (
     <div className={classNames("", props.className)}>
       <Image
         priority
         alt="Mountains"
-        src={'/bg.svg'}
-        // src={BG}
-        // quality={100}
+        // src={'/bg.svg'}
+        src={BG}
+        quality={100}
         fill
         className="object-cover"
-        // style={{
-        //   objectFit: 'cover',
-        // }}
+        style={{
+          objectFit: 'cover',
+        }}
       />
       {props.children}
     </div>
