6b1edc109b717f108c7d55d30abe48154f566388
plh97 <pengliheng111@gmail.com>
2023-12-23 23:35:30 +0800

    feat: add pwa

--- components/Viewer/index.tsx
+++ components/Viewer/index.tsx
@@ -19,15 +19,13 @@ export default function Viewer({ data, title = "" }: IProps) {
 			<CssBackground className={classNames(styles["container"], 'h-72 max-md:h-40')}>
 				<h1 className="max-md:text-2xl md:text-4xl z-10">{title}</h1>
 				<div className={classNames(styles["detail"], 'z-10 p-4 max-md:p-2 gap-x-4 max-md:gap-x-2')}>
-					{data?.avatarUrl && (
-						<Image
-							className={classNames(styles["image"], 'max-md:!h-12 max-md:!w-12')}
-							alt="avatar"
-							width={64}
-							height={64}
-							src={data?.avatarUrl}
-						/>
-					)}
+					<Image
+						className={classNames(styles["image"], 'max-md:!h-12 max-md:!w-12')}
+						alt="avatar"
+						width={64}
+						height={64}
+						src={data?.avatarUrl}
+					/>
 					<span className={styles["detail-list"]}>
 						<span className='max-md:text-2xl md:text-4xl'>{data.name}</span>
 						<span className="max-md:text-xs md:text-md">{data.bio}</span>
