fe7a7cc60e84f9c12380cca6888c003b6ca8dd7a
plh97 <pengliheng111@gmail.com>
2023-12-23 23:55:35 +0800

    feat: add pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,35 +49,35 @@ export default function RootLayout({
         <meta name="description" content="Best PWA App in the world" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
 
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:url" content="https://plhh.xyz" />
         <meta name="twitter:title" content="PWA App" />
         <meta name="twitter:description" content="Best PWA App in the world" />
-        <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta name="twitter:image" content="https://plhh.xyz/icons/android-chrome-192x192.png" />
+        <meta name="twitter:creator" content="@plh97" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="PWA App" />
-        <meta property="og:description" content="Best PWA App in the world" />
-        <meta property="og:site_name" content="PWA App" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+        <meta property="og:title" content="Blog" />
+        <meta property="og:description" content="Peng's Personal Blog" />
+        <meta property="og:site_name" content="Blog" />
+        <meta property="og:url" content="https://plhh.xyz" />
+        <meta property="og:image" content="https://plhh.xyz/icons/apple-touch-icon.png" />
+
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+        />
+
       </head>
       <Script
         async
