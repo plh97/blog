e6142de1d7d3097612531677f8a878b98663a8cd
plh97 <pengliheng111@gmail.com>
2024-01-07 16:07:16 +0800

    feat: add img

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,12 +9,16 @@ export default async function PageNotFound() {
       <Image
         width="522"
         height="237"
-        className={styles["img"]}
+        className="w-4/5"
         alt="not_found"
         src={"/404_img.svg"}
       />
-      <p className="mt-4">Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <p className="mt-8">Could not find requested resource</p>
+      <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href="/">
+          Return Home
+        </Link>
+      </button>
     </div>
   );
 }
